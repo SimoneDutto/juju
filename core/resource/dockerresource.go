08ca404a1b7b40fa50bf8c8ad15550efda5bf983
SimoneDutto <simone.dutto@canonical.com>
2026-06-29 14:09:44 +0200

    fix: add yaml tags to docker structs

--- core/resource/dockerresource.go
+++ core/resource/dockerresource.go
@@ -42,13 +42,13 @@ func (t *Token) Content() string {
 // BasicAuthConfig contains authorization information for basic auth.
 type BasicAuthConfig struct {
 	// Auth is the base64 encoded "username:password" string.
-	Auth *Token
+	Auth *Token `json:"auth,omitempty" yaml:"auth,omitempty"`
 
 	// Username holds the username used to gain access to a non-public image.
-	Username string
+	Username string `json:"username" yaml:"username"`
 
 	// Password holds the password used to gain access to a non-public image.
-	Password string
+	Password string `json:"password" yaml:"password"`
 }
 
 // Empty checks if the auth information is empty.
@@ -60,14 +60,14 @@ func (ba BasicAuthConfig) Empty() bool {
 // Juju does not support the docker credential helper because k8s does not support it either.
 // https://kubernetes.io/docs/concepts/containers/images/#configuring-nodes-to-authenticate-to-a-private-registry
 type TokenAuthConfig struct {
-	Email string
+	Email string `json:"email,omitempty" yaml:"email,omitempty"`
 
 	// IdentityToken is used to authenticate the user and get
 	// an access token for the registry.
-	IdentityToken *Token
+	IdentityToken *Token `json:"identitytoken,omitempty" yaml:"identitytoken,omitempty"`
 
 	// RegistryToken is a bearer token to be sent to a registry
-	RegistryToken *Token
+	RegistryToken *Token `json:"registrytoken,omitempty" yaml:"registrytoken,omitempty"`
 }
 
 // Empty checks if the auth information is empty.
@@ -77,17 +77,17 @@ func (ac TokenAuthConfig) Empty() bool {
 
 // ImageRepoDetails contains authorization information for connecting to a Registry.
 type ImageRepoDetails struct {
-	BasicAuthConfig
-	TokenAuthConfig
+	BasicAuthConfig `json:",inline" yaml:",inline"`
+	TokenAuthConfig `json:",inline" yaml:",inline"`
 
 	// Repository is the namespace of the image repo.
-	Repository string
+	Repository string `json:"repository,omitempty" yaml:"repository,omitempty"`
 
 	// ServerAddress is the auth server address.
-	ServerAddress string
+	ServerAddress string `json:"serveraddress,omitempty" yaml:"serveraddress,omitempty"`
 
 	// Region is the cloud region.
-	Region string
+	Region string `json:"region,omitempty" yaml:"region,omitempty"`
 }
 
 // IsPrivate checks if the repository detail is private.
@@ -98,9 +98,9 @@ func (rid ImageRepoDetails) IsPrivate() bool {
 // DockerImageDetails holds the details for a Docker resource type.
 type DockerImageDetails struct {
 	// RegistryPath holds the path of the Docker image (including host and sha256) in a docker registry.
-	RegistryPath string
+	RegistryPath string `json:"ImageName" yaml:"registrypath"`
 
-	ImageRepoDetails
+	ImageRepoDetails `json:",inline" yaml:",inline"`
 }
 
 // IsPrivate shows if the image repo is private or not.
